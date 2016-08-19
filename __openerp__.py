085bfa4e4abdbd1370ac73f0a010e385bf395b40
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:47:39 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "PDF on contracts",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['sale_contract'],
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
